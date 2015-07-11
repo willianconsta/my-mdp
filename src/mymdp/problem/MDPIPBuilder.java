f77f260463d3ce3a7d3411d1729aa034c605501e
Willian Constâncio da Silva <willian@willian.com>
2015-07-11 17:21:13 -0300

    Documentation, createFromMDP

--- src/mymdp/problem/MDPIPBuilder.java
+++ src/mymdp/problem/MDPIPBuilder.java
@@ -6,36 +6,63 @@ import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Iterables.getOnlyElement;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.Range;
 
 import mymdp.core.Action;
+import mymdp.core.MDP;
 import mymdp.core.MDPIP;
 import mymdp.core.State;
 import mymdp.util.Trio;
 
-import com.google.common.collect.Range;
-
 public class MDPIPBuilder
 {
-	private final Map<String,StateImpl> states;
-	private final Map<StateImpl,Double> rewards;
-	private final Map<Action,Map<State,Map<State,String>>> transitions;
+	private final Map<String,StateImpl> states = new LinkedHashMap<>();
+	private final Map<StateImpl,Double> rewards = new LinkedHashMap<>();
+	private final Map<Action,Map<State,Map<State,String>>> transitions = new LinkedHashMap<>();
 	private double discountRate;
-	private Set<String> restrictions;
-	private final Map<Trio<State,Action,State>,String> vars;
+	private Set<String> restrictions = new LinkedHashSet<>();
+	private final Map<Trio<State,Action,State>,String> vars = new LinkedHashMap<>();
 	private int i = 0;
 
-	public MDPIPBuilder() {
-		states = new LinkedHashMap<>();
-		rewards = new LinkedHashMap<>();
-		transitions = new LinkedHashMap<>();
-		vars = new LinkedHashMap<>();
-		restrictions = new LinkedHashSet<>();
+	private MDPIPBuilder() {
+	}
+
+	public static MDPIPBuilder newBuilder() {
+		return new MDPIPBuilder();
+	}
+
+	public static MDPIP createFromMDP(final MDP mdp, final ImprecisionGenerator generator) {
+		final MDPIPBuilder builder = newBuilder()
+				.states(mdp.getStates().stream().map(State::name).collect(Collectors.toSet()))
+				.discountRate(mdp.getDiscountFactor())
+				.rewards(mdp.getStates().stream().collect(Collectors.toMap(State::name, s -> mdp.getRewardFor(s))));
+
+		for ( final Action a : mdp.getAllActions() ) {
+			final Set<String[]> transitionDefs = new HashSet<>();
+			for ( final State s : mdp.getStates() ) {
+				if ( !a.isApplicableTo(s) ) {
+					continue;
+				}
+
+				for ( final Entry<State,Double> transition : mdp.getPossibleStatesAndProbability(s, a) ) {
+					final Range<Double> range = generator.generateRange(s.name(), a.name(), transition.getKey().name(), transition.getValue());
+					transitionDefs.add(new String[]{s.name(), transition.getKey().name(), range.lowerEndpoint().toString(),
+							range.upperEndpoint().toString()});
+				}
+			}
+			builder.action(a.name(), transitionDefs);
+		}
+
+		return builder.build();
 	}
 
 	public MDPIPBuilder states(final Set<String> stateDefs) {
@@ -45,7 +72,20 @@ public class MDPIPBuilder
 		return this;
 	}
 
-	public MDPIPBuilder actions(final String actionName, final Set<String[]> transitionDefs) {
+	/**
+	 * 
+	 * @param actionName
+	 * @param transitionDefs
+	 *            0th index current state, 1st index next state, 2nd and 3rd indexes are double values related to the probability.
+	 *            <ul>
+	 *            <li>If 2nd index is equal to 3rd index then the value must be a double and is the value of the probability of transition</li>
+	 *            <li>If 2nd index is not equal to "0.0" then a restriction of var >= (value of 2nd index) is created.</li>
+	 *            <li>If 3rd index is not equal to "1.0" then a restriction of var <= (value of 3rd index) is created.</li>
+	 *            <li>All variables related to a current state must sum one.</li>
+	 *            </ul>
+	 * @return
+	 */
+	public MDPIPBuilder action(final String actionName, final Set<String[]> transitionDefs) {
 		final Set<State> appliableStates = new LinkedHashSet<>();
 		final Action action = new ActionImpl(actionName, appliableStates);
 		final Map<State,String> sumOnePerState = new LinkedHashMap<>();
@@ -79,22 +119,15 @@ public class MDPIPBuilder
 			if ( transitionDef[2].equals(transitionDef[3]) ) {
 				map.put(state2, transitionDef[3]);
 				continue;
-			} else {
-				if ( !transitionDef[2].equals("0.0") ) {
-					restrictions.add(var + " >= " + transitionDef[2]);
-				}
-				if ( !transitionDef[3].equals("1.0") ) {
-					restrictions.add(var + " <= " + transitionDef[3]);
-				}
 			}
-			String restrOne = sumOnePerState.get(state1);
-			if ( restrOne != null ) {
-				restrOne += "+";
-			} else {
-				restrOne = "";
+
+			if ( !transitionDef[2].equals("0.0") ) {
+				restrictions.add(var + " >= " + transitionDef[2]);
 			}
-			restrOne += var;
-			sumOnePerState.put(state1, restrOne);
+			if ( !transitionDef[3].equals("1.0") ) {
+				restrictions.add(var + " <= " + transitionDef[3]);
+			}
+			sumOnePerState.merge(state1, var, (oldValue, newValue) -> oldValue + "+" + newValue);
 			vars.put(Trio.of(state1, action, state2), var);
 		}
 
@@ -129,7 +162,7 @@ public class MDPIPBuilder
 		return this;
 	}
 
-	public MDPIPBuilder reward(final Map<String,Double> rewards) {
+	public MDPIPBuilder rewards(final Map<String,Double> rewards) {
 		for ( final Entry<String,Double> entry : rewards.entrySet() ) {
 			this.rewards.put(states.get(entry.getKey()), entry.getValue());
 		}
