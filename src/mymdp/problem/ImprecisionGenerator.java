0be3616d72de2944f7e6ca117fa62cc03972e942
Willian Constâncio da Silva <willian@willian.com>
2015-07-11 17:17:23 -0300

    Javadoc

--- src/mymdp/problem/ImprecisionGenerator.java
+++ src/mymdp/problem/ImprecisionGenerator.java
@@ -4,5 +4,14 @@ import com.google.common.collect.Range;
 
 public interface ImprecisionGenerator
 {
-	Range<Double> generateRange(String initial, String action, String next, double actualProb);
+	/**
+	 * Generates a range of the given probability for a trio [initial state, action, next state].
+	 * 
+	 * @param initialState
+	 * @param action
+	 * @param nextState
+	 * @param actualProbability
+	 * @return
+	 */
+	Range<Double> generateRange(String initialState, String action, String nextState, double actualProbability);
 }
